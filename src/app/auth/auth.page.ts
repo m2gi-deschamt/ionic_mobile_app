04c18da4d847172887c55965ca1f10724f82e135
skander23000 <skanderchouiter@gmail.com>
2025-03-12 22:08:07 +0100

    Fix bug login

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonItem, IonLabel, IonCardContent ,IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonItem, 
+         IonLabel, IonCardContent, IonButton, IonInput } from '@ionic/angular/standalone';
 import { AuthService } from '../services/auth/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import {sendEmailVerification} from '@angular/fire/auth'
+import { sendEmailVerification } from '@angular/fire/auth'
 import { UserService } from '../services/user/user.service';
 
 const INVALID_CRDENTIALS = "INVALID_LOGIN_CREDENTIALS";
@@ -14,14 +15,25 @@ const INVALID_CRDENTIALS = "INVALID_LOGIN_CREDENTIALS";
   selector: 'app-auth',
   templateUrl: './auth.page.html',
   styleUrls: ['./auth.page.scss'],
-  imports: [IonHeader, IonToolbar, IonCardContent, IonTitle, IonContent, IonCard, IonItem, IonLabel, IonButton, ReactiveFormsModule, CommonModule],
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonCardContent, 
+    IonTitle, 
+    IonContent, 
+    IonCard, 
+    IonItem, 
+    IonLabel, 
+    IonButton, 
+    IonInput, 
+    ReactiveFormsModule, 
+    CommonModule
+  ],
 })
-
 export class AuthPage implements OnInit {
   authForm!: FormGroup;
   isLogin = true;
   errorMessage = '';
-  
 
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
@@ -29,19 +41,32 @@ export class AuthPage implements OnInit {
   private router = inject(Router);
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
     this.errorMessage = '';
-    this.authForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      username: ['', [Validators.required, Validators.minLength(3)]],
-      ...(this.isLogin ? {} : { confirmPassword: ['', Validators.required] })
-    });
+    
+    if (this.isLogin) {
+      this.authForm = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required]
+      });
+    } else {
+      this.authForm = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        username: ['', [Validators.required, Validators.minLength(3)]],
+        confirmPassword: ['', Validators.required]
+      });
+    }
   }
 
   toggleMode(): void {
     this.isLogin = !this.isLogin;
-    this.ngOnInit();
+    this.initForm();
   }
+
 
   async onSubmit() {
     if (this.authForm.invalid) {
