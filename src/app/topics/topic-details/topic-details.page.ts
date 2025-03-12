f8af7ecad7a599ba6940b93e454e78e6a603b9f2
skander23000 <skanderchouiter@gmail.com>
2025-03-12 22:37:24 +0100

    topic details not working but ui fixed

--- src/app/topics/topic-details/topic-details.page.ts
+++ src/app/topics/topic-details/topic-details.page.ts
@@ -1,8 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonToolbar, IonBreadcrumbs, IonTitle, IonButton, IonImg, IonFab, IonFabButton, IonContent, IonList, IonItem, IonLabel, IonIcon } from '@ionic/angular/standalone';
-import { TopicService } from 'src/app/services/topic/topic.service';
+import { IonicModule } from '@ionic/angular';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ModalController } from '@ionic/angular/standalone';
 import { PopoverController } from '@ionic/angular/standalone';
@@ -12,16 +11,80 @@ import { addIcons } from 'ionicons';
 import { addOutline, chevronForward, ellipsisVertical } from 'ionicons/icons';
 import { ItemManagementPopover } from '../popover/item-management/item-management.component';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { TopicService } from 'src/app/services/topic/topic.service';
 
 addIcons({ addOutline, chevronForward, ellipsisVertical });
 
 @Component({
   selector: 'app-topic-details',
-  templateUrl: './topic-details.page.html',
-  styleUrls: ['./topic-details.page.scss'],
+  template: `
+    <ion-header [translucent]="true">
+      <ion-toolbar>
+        <ion-breadcrumbs>
+          <ion-breadcrumb routerLink="">Topics</ion-breadcrumb>
+          <ion-breadcrumb [routerLink]="'#topics/' + topic()?.id">{{
+            topic()?.name
+          }}</ion-breadcrumb>
+        </ion-breadcrumbs>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content [fullscreen]="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">{{ topic()?.name }}</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-list>
+        @for(post of topic()?.posts; track post.id) {
+
+        <ion-item>
+          <ion-button
+            slot="start"
+            fill="clear"
+            id="click-trigger"
+            (click)="presentPostManagementPopover($event, post)"
+            aria-label="open post management popover"
+            data-cy="open-post-management-popover"
+            ><ion-icon
+              slot="icon-only"
+              color="medium"
+              name="ellipsis-vertical"
+            ></ion-icon
+          ></ion-button>
+          <ion-label>{{ post.name }}</ion-label>
+        </ion-item>
+
+        } @empty {
+        <ion-img
+          class="image"
+          src="assets/img/no_data.svg"
+          alt="No data"
+        ></ion-img>
+        }
+      </ion-list>
+      <ion-fab slot="fixed" vertical="bottom" horizontal="end">
+        <ion-fab-button
+          data-cy="open-create-post-modal-button"
+          aria-label="open add post modal"
+          (click)="openModal()"
+        >
+          <ion-icon name="add-outline"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  `,
+  styles: [
+    `
+      .image::part(image) {
+        width: 50%;
+        margin: auto;
+      }
+    `,
+  ],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonBreadcrumbs, IonTitle, IonButton, IonImg, IonFab, 
-    IonFabButton, IonContent, IonList, IonItem, IonLabel, IonIcon, CommonModule, FormsModule, RouterLink],
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
 })
 export class TopicDetailsPage {
   private readonly topicService = inject(TopicService);
@@ -32,9 +95,6 @@ export class TopicDetailsPage {
   topicId = this.route.snapshot.params['id'];
 
   topic = toSignal(this.topicService.getById(this.topicId));
-
-  posts = toSignal(this.topicService.getPostsByTopicId(this.topicId));
-
 
   async openModal(post?: Post): Promise<void> {
     const modal = await this.modalCtrl.create({
@@ -58,7 +118,7 @@ export class TopicDetailsPage {
       data: { action },
     } = await popover.onDidDismiss();
 
-    //console.log(action);
+    console.log(action);
 
     if (action === 'remove') this.topicService.removePost(this.topicId, post);
     else if (action === 'edit') this.openModal(post);
