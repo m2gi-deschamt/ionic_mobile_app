b27ec7e1652d9210550ba9f21f9a7e8069d73bce
skander23000 <skanderchouiter@gmail.com>
2025-03-19 15:17:38 +0100

    changed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBNgghXk14g0lDL2CC6UZn2s867bSfzKXI",
+    authDomain: "ionic-mobile-app-91156.firebaseapp.com",
+    projectId: "ionic-mobile-app-91156",
+    storageBucket: "ionic-mobile-app-91156.firebasestorage.app",
+    messagingSenderId: "47848942194",
+    appId: "1:47848942194:web:d56b36abeab85f021070eb"
+  }
 };
